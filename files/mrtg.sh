b05d0fa234d06a49515ae45931d33ebdc64c6356
Frederico Freire Boaventura <frederico@boaventura.net>
2021-09-03 09:41:19 -0300

    Adding port support and fix Lighttpd PID reference

--- files/mrtg.sh
+++ files/mrtg.sh
@@ -10,11 +10,11 @@ MRTGCFG=${MRTGDIR}/mrtg.cfg
 if [ -n "${HOSTS}" ]; then
     hosts=$(echo ${HOSTS} | tr '[,;]' ' ')
     for asset in ${hosts}; do
-        read -r COMMUNITY HOST VERSION < <(echo ${asset} | sed -e 's/:/ /g')
+        read -r COMMUNITY HOST VERSION PORT < <(echo ${asset} | sed -e 's/:/ /g')
         # COMMUNITY=$(echo $asset | cut -d: -f1)
         # HOST=$(echo $asset | cut -d: -f2)
         if [[ "${VERSION}" -eq "2" || -z "${VERSION}" ]]; then _snmp_ver="2c"; else _snmp_ver=${VERSION}; fi
-        NAME=$(snmpwalk -Oqv -v${_snmp_ver} -c ${COMMUNITY} ${HOST} .1.3.6.1.2.1.1.5)
+        NAME=$(snmpwalk -Oqv -v${_snmp_ver} -c ${COMMUNITY} ${HOST}:${PORT:-"161"} .1.3.6.1.2.1.1.5)
         if [ -z "${NAME}" ]; then
             NAME="${HOST}"
         fi
@@ -24,15 +24,16 @@ if [ -n "${HOSTS}" ]; then
             --global "Options[_]: growright, bits" \
             --global "EnableIPv6: ${ENABLE_V6}" \
             --global "LogFormat: rrdtool" \
-            --snmp-options=:::::${VERSION:-2} \
+            --snmp-options=:${PORT:-"161"}::::${VERSION:-"2"} \
             --output=${MRTGDIR}/conf.d/${NAME}.cfg "${COMMUNITY}@${HOST}"
     done
 else
     COMMUNITY=${1:-"public"}
     HOST=${2:-"localhost"}
     VERSION=${3:-"2"}
+    PORT=${4:-"161"}
     if [[ "${VERSION}" -eq "2" || -z "${VERSION}" ]]; then _snmp_ver="2c"; else _snmp_ver=${VERSION}; fi
-    NAME=$(snmpwalk -Oqv -v${_snmp_ver} -c ${COMMUNITY} ${HOST} .1.3.6.1.2.1.1.5)
+    NAME=$(snmpwalk -Oqv -v${_snmp_ver} -c ${COMMUNITY} ${HOST}:${PORT} .1.3.6.1.2.1.1.5)
     if [ -z "${NAME}" ]; then
         NAME="${HOST}"
     fi
@@ -41,7 +42,7 @@ else
             --global "Options[_]: growright, bits" \
             --global "EnableIPv6: ${ENABLE_V6}" \
             --global "LogFormat: rrdtool" \
-            --snmp-options=:::::${VERSION:-2} \
+            --snmp-options=:${PORT}::::${VERSION} \
             --output=${MRTGDIR}/conf.d/${NAME}.cfg "${COMMUNITY}@${HOST}"
 fi
 
@@ -53,10 +54,7 @@ env LANG=C /usr/bin/mrtg ${MRTGCFG}
 /usr/bin/indexmaker --columns=1 ${MRTGCFG} > ${WEBDIR}/index.html
 chown -R lighttpd:lighttpd ${WEBDIR}
 
-# /usr/sbin/nginx &
-# NGINXID=$!
-
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf &
+/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -D &
 HTTPID=$!
 
 /usr/sbin/crond -f -L /proc/self/fd/1 -l debug &
